d750485fc1ca5f3687e1947bcd08fab2be4508df
0x1618 <80158535+0x1618@users.noreply.github.com>
2025-10-19 15:45:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='codetable',
-    version='1.0.7',
+    version='1.0.8',
     description=
     "Codetable is a lightweight package for seamlessly setting up codes, such as those used in API responses.",
     long_description=long_description,
